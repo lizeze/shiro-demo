d869dfc7d666ee2e3d5bfabd97fca77e661c234c
zeze.li <lizeze.boy@gmail.com>
2019-10-14 17:23:58 +0800

    first commit

--- src/main/java/com/lzz/shirodemo/shiro/ShiroConfig.java
+++ src/main/java/com/lzz/shirodemo/shiro/ShiroConfig.java
@@ -1,10 +1,15 @@
 package com.lzz.shirodemo.shiro;
 
+import org.apache.shiro.session.SessionListener;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @program: shiro-demo
@@ -23,7 +28,7 @@ public class ShiroConfig {
 
     @Bean(name = "securityManager")
     public DefaultWebSecurityManager defaultWebSecurityManager(CustomRealm customRealm) {
-        DefaultWebSecurityManager  securityManager = new DefaultWebSecurityManager ();
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(customRealm);
         return securityManager;
     }
@@ -39,4 +44,17 @@ public class ShiroConfig {
         shiroFilterFactoryBean.setSecurityManager(securityManager);
         return shiroFilterFactoryBean;
     }
+
+//    @Bean
+//    public DefaultWebSessionManager sessionManager() {
+//        DefaultWebSessionManager sessionManager = new DefaultWebSessionManager();
+//        Collection<SessionListener> listeners = new ArrayList<>();
+//        listeners.add(new ShiroSessionListener());
+//        // 设置 session超时时间
+//        sessionManager.setGlobalSessionTimeout(6 * 1000L);
+//        sessionManager.setSessionListeners(listeners);
+//        sessionManager.setSessionIdUrlRewritingEnabled(false);
+//        return sessionManager;
+//    }
+
 }
